a768be4c71b61015a80b54b7130f47dc774a6a8a
Florence Kyo Gado <155133363+kyogado18@users.noreply.github.com>
2025-05-25 21:37:40 +0800

    landing change

--- clothingsm/resources/views/LandingPage.blade.php
+++ clothingsm/resources/views/LandingPage.blade.php
@@ -130,6 +130,6 @@
     </div>
   </div>
   <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-  <script src="js/slidescript.js"></script>
+  <script src="{{asset("js/slidescript.js")}}"></script>
 </body>
 </html>
